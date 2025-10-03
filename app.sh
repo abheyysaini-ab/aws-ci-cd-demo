dcc5f75ed3a6bc6e7d849f2be3bf08dca63dad8c
abheyysaini-ab <abheyysaini@gmail.com>
2025-10-04 04:15:44 +0530

    Update app.sh

--- app.sh
+++ app.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-echo "✅ Deployed via GitHub Actions at $(date)" | sudo tee -a /home/ec2-user/deploy.log
+
+APP_DIR="/home/ec2-user/aws-ci-cd-demo"
+
+# If the app directory does not exist, clone it
+if [ ! -d "$APP_DIR" ]; then
+  echo "Cloning repo for the first time..."
+  git clone https://github.com/abheyysaini-ab/aws-ci-cd-demo.git $APP_DIR
+else
+  echo "Repo already exists. Pulling latest changes..."
+  cd $APP_DIR
+  git pull origin main
+fi
+
+echo "✅ Deployed via GitHub Actions at $(date)" >> /home/ec2-user/deploy.log
