05a892f269df48985b2200bb8d2cd4e382437595
abheyysaini-ab <abheyysaini@gmail.com>
2025-11-10 23:52:37 +0530

    Update app.sh

--- app.sh
+++ app.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-APP_DIR="/home/ec2-user/aws-ci-cd-demo"
+# Use Jenkins workspace
+APP_DIR="$WORKSPACE/aws-ci-cd-demo"
+LOG_FILE="$WORKSPACE/deploy.log"
 
 # If the app directory does not exist, clone it
 if [ ! -d "$APP_DIR" ]; then
   echo "Cloning repo for the first time..."
-  git clone https://github.com/abheyysaini-ab/aws-ci-cd-demo.git $APP_DIR
+  git clone https://github.com/abheyysaini-ab/aws-ci-cd-demo.git "$APP_DIR"
 else
   echo "Repo already exists. Pulling latest changes..."
-  cd $APP_DIR
+  cd "$APP_DIR"
   git pull origin main
 fi
 
-echo "✅ Deployed via GitHub Actions at $(date)" >> /home/ec2-user/deploy.log
+echo "✅ Deployed via Jenkins at $(date)" >> "$LOG_FILE"
